7db2188dd6d8e8dc1ffad3149b89e65c90d8b2f6
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2022-12-10 10:57:49 +0330

    Configure React app for deployment to GitHub Pages

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,8 +6,8 @@ const HomePage: React.FC<IProps> = (props) => {
   return (
     <>
       <h1>This is a frontend microservice architecture</h1>
-      <h3>This app consists of three separate parts(aka micofrontend)</h3>
-      <div id="part-two">Part Two</div>
+      <h3>This app consists of three separate parts(aka microfrontend)</h3>
+      <div id="part-two">Part Two in ReactJS</div>
       <h3>
         Powered by:{" "}
         <a href="https://single-spa.js.org/">https://single-spa.js.org/</a>
